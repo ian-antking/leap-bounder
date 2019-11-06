94ef2fb546d0505615ad989eda5f50c5078a369a
Ian King <ian.antking@gmail.com>
2019-11-06 09:27:45 +0000

    added walking

--- src/states/player/walk.js
+++ src/states/player/walk.js
@@ -4,6 +4,16 @@ class WalkState extends State {
   constructor(name, sprite) {
     super(name, sprite);
   }
+
+  enter(command) {
+    const direction = command.name.includes('left') ? 'left' : 'right';
+    this.sprite.move(direction);
+  }
+
+  execute(command) {
+    const { name } = command;
+    return name === 'leftUp' || name === 'rightUp' ? 'stand' : this.name;
+  }
 }
 
 export default WalkState;
