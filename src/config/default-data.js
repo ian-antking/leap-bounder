3b7a35806639bf179b514a22357d90f174b015bc
Ian King <ian.antking@gmail.com>
2019-11-09 11:29:00 +0000

    added spawnPoint and checkpoints

---
+++ src/config/default-data.js
@@ -0,0 +1,8 @@
+const defaultConfig = {
+  spawn: {
+    x: 50,
+    y: 50,
+  },
+};
+
+export default defaultConfig;
--- src/scenes/gamescene.js
+++ src/scenes/gamescene.js
@@ -1,11 +1,16 @@
 import 'phaser';
 import Player from '../sprites/player';
 import Command from '../utils/command';
+import defaultConfig from '../config/default-data';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
     this.commands = [];
+  }
+
+  init(data) {
+    this.data = data;
   }
 
   createKeyboardListener(keyName, commandName = keyName) {
@@ -20,6 +25,27 @@ export default class GameScene extends Phaser.Scene {
     });
   }
 
+  changeSpawnFlag(spawnTile) {
+    this.spawnLayer.putTileAt(189, spawnTile.x, spawnTile.y);
+  }
+
+  findSpawn(layer) {
+    const spawn = this.data.spawn ? (
+      layer.getTileAtWorldXY(this.data.spawn.x, this.data.spawn.y)
+    ) : (
+      layer.findByIndex(189)
+    );
+    this.setSpawn(spawn);
+  }
+
+  setSpawn(spawnTile) {
+    this.spawn = {
+      x: spawnTile.pixelX,
+      y: spawnTile.pixelY + 48,
+    };
+    this.changeSpawnFlag(spawnTile);
+  }
+
   create() {
     this.map = this.make.tilemap({ key: 'dev-map' });
     this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
@@ -27,8 +53,13 @@ export default class GameScene extends Phaser.Scene {
     this.mapTiles = this.map.addTilesetImage('tilesheet_complete');
     this.groundLayer = this.map.createStaticLayer('ground', this.mapTiles)
       .setCollisionByProperty({ collides: true });
+    this.spawnLayer = this.map.createDynamicLayer('spawn', this.mapTiles);
     this.waterLayer = this.map.createDynamicLayer('water', this.mapTiles)
       .setDepth(5);
+
+    this.findSpawn(this.spawnLayer);
+
+    this.spawnLayer.setTileIndexCallback(190, (_, spawnTile) => this.setSpawn(spawnTile), this);
 
     this.input.keyboard.on('keydown_' + 'SPACE', () => {
       this.player.flipGravity();
@@ -37,23 +68,25 @@ export default class GameScene extends Phaser.Scene {
     this.createKeyboardListener('left');
     this.createKeyboardListener('right');
 
-
     this.player = new Player({
       scene: this,
       key: 'player',
-      x: 50,
-      y: 50,
+      x: this.spawn.x,
+      y: this.spawn.y,
     });
 
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
     this.cameras.main.startFollow(this.player);
 
     this.physics.add.collider(this.player, this.groundLayer);
+    this.physics.add.overlap(this.player, this.spawnLayer);
   }
 
   update() {
     if (!this.player.alive) {
-      this.scene.restart();
+      this.scene.restart({
+        spawn: this.spawn,
+      });
     }
     this.player.update(this.commands.shift());
   }
